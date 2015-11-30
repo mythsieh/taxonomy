7e1c3604e737fda38ba154445b01dbfb32680470
Mike Hsieh <mythsieh@gmail.com>
2015-11-30 12:38:37 -0800

    Complete taxonomy

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,17 @@
 var Bee = function() {
+  //invoke Grub with this object as the context
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+// This creates a copy of the prototype of Grub and makes the Grub prototype it's prototype 
+Bee.prototype = Object.create(Grub.prototype);
+// However, this makes the Grub prototype the constructor value and so checking the constructor would point to  Grub instead
+
+// This sets the constructor function of the prototype to the Bee function
+Bee.prototype.constructor = Bee;
+
+//instance of any bee should return true for bee and grub
 
--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.color = 'yellow';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+//Set the prototypal chain to equal a copy of the Bee.prototypal chain
+ForagerBee.prototype = Object.create(Bee.prototype);
+// Reset constructor to point back to prototype
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function(treasure){
+  this.treasureChest.push(treasure);
+}
